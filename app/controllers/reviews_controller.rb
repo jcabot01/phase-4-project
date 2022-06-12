caf6a926ad4ae6e92be6b2ceab4f86122705cbf3
JonCabot <jfc0053@gmail.com>
2022-06-12 07:18:09 -0500

    controllers are in

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,49 @@
 class ReviewsController < ApplicationController
+  #GET
+  ##index /reviews
+  def index 
+    reviews = Review.all
+    render json: reviews, status: :ok
+  end
+
+  #GET
+  ##show   /reviews/:id
+  def show 
+    review = find_review
+    render json: review, status: :ok
+  end
+
+  #POST
+  ##create   /reviews
+  def create
+    review = Review.create!(review_params)
+    render json: review, status: :accepted
+  end
+
+  #PATCH
+  ##update    /reviews/:id
+  def update
+    review = find_review
+    review.update!(review_params)
+    render json: review, status: :accepted
+  end
+
+  #DELETE
+  ##destroy    /reviews/:id
+  def destroy
+    review = find_review
+    review.destroy
+    head :no_content
+  end
+
+  private
+
+  def find_review
+    Review.find(params[:id])
+  end
+
+  def review_params
+    params.permit(:review)
+  end
+  
 end
--- app/controllers/rvs_controller.rb
+++ app/controllers/rvs_controller.rb
@@ -1,2 +1,16 @@
 class RvsController < ApplicationController
+  #GET
+  ##index /rvs
+  def index 
+    rvs = Rv.all
+    render json: rvs, status: :ok
+  end
+
+  #GET
+  ##show   /rvs/:id
+  def show 
+    rv = Rv.find(params[:id])
+    render json: rv, status: :ok
+  end
+  
 end
