799d8b15efa80fb1151cd95bf2aabaa226a3b186
JonCabot <jfc0053@gmail.com>
2022-06-12 17:13:48 -0500

    working the MUI react bug

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import { Button } from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-      </header>
+    <div>
+    <Button> Hello </Button>
+    <Router>
+      <Routes>
+        <Route path ='/login' element={<LoginPage />}/>
+        <Route path ="*" element="404 Page Not Found"/>
+      </Routes>
+    </Router>
     </div>
-  );
+    
+  )
 }
 
 export default App;
---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+// import { TextField } from '@mui/material';
+// import { Container } from '@mui/material';
+
+function LoginPage() {
+
+  return(
+    <>
+   Login Page goes here!!!
+    </>
+  )
+};
+
+export default LoginPage
